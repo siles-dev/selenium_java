ed0f6d97cc4b32f8e0cd6c9aac4b5f15684d6e10
Siles <siles@Siless-MacBook-Pro.local>
2023-04-26 21:19:16 -0700

    Implemented TestNg to get test results reports

--- src/test/java/co/platform/sampleTest/OpenBrowserTest.java
+++ src/test/java/co/platform/sampleTest/OpenBrowserTest.java
@@ -1,19 +1,36 @@
 package co.platform.sampleTest;
 
 import co.platform.selenium.driver.Driver;
-import org.openqa.selenium.WebDriver;
+import com.google.common.base.Verify;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class OpenBrowserTest {
-    public static void main(String [] args) {
-        //Use the driver class to set up testing browser
-        Driver webDriver = new Driver("chrome","http://www.google.com");
+    private Driver webDriver;
 
+    @BeforeClass
+    public void initBrowser() {
+        webDriver = new Driver("chrome","http://www.google.com");
+    }
+
+    //Adding `priority` since default order is alphabetical, and we want the failed assertion to be executed first
+    @Test(priority = 0)
+    public void verifyTypeAssertionTest() {
         String actualTitle = webDriver.getWebDriver().getTitle();
-        if (actualTitle.contentEquals("Google")){
-            System.out.println("Test Passed! title matches \"Google\"");
-        } else {
-            System.out.println("Test Failed with " + actualTitle);
-        }
+        Verify.verify(actualTitle.equals("Wrong String"),
+                "The test will continue although test failed with this `verify` assertion");
+    }
+
+    @Test(priority = 1)
+    public void assertTypeAssertionPassTest() {
+        String actualTitle = webDriver.getWebDriver().getTitle();
+        Assert.assertEquals(actualTitle,"Google");
+    }
+
+    @AfterClass
+    public void tearDown() {
         webDriver.cleanUp();
     }
 }
